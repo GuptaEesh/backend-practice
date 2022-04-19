c3e18af77ecd4dcca5a8754422545f622e592e5b
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-04-19 10:32:32 +0530

    minor refactor

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const subscribersRouter = require("./routes/subscribers");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to database"));
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to database"));
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/subscribers", subscribersRouter);
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  .then(() => console.log("Successful"))
+  .catch((err) => console.log(err));
 app.listen(PORT, () => console.log("index started"));
