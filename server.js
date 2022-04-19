a33e2319f06c5391ac9799450528339b9069c1c2
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-04-19 10:14:55 +0530

    update console message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const subscribersRouter = require("./routes/subscribers");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to db"));
+db.once("open", () => console.log("Connected to database"));
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
