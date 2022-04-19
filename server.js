358ef6af555ea50242660be979a96cc8f291b4d4
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-04-19 09:36:42 +0530

    fix: minor corrections in package.json

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to db"));
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use("/subscribers", subscribersRouter);
-app.listen(3000, () => console.log("Server started"));
+app.listen(PORT, () => console.log("Server started"));
